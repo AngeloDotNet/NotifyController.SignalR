94a7fe58940df4b8fdaeeb7eac87cbfd3d1f1503
Angelo Pirola <angelo@aepserver.it>
2021-08-29 16:50:23 +0200

    Aggiunto invio messaggio SignalR in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,23 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Chat_SignalR.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IHubContext<ChatHub> signalR;
+
+        public HomeController(IHubContext<ChatHub> signalR)
+        {
+            this.signalR = signalR;
+        }
+
+        public async Task<IActionResult> Index()
         {
             ViewData["Title"] = "Benvenuto!";
+            await signalR.Clients.All.SendAsync("ReceiveMessage", "Server", "Prova messaggio !");
             return View();
         }
     }
-
 }
